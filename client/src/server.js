b3dd03f38e74eaa7d7e6cf6ac8c33bbe91e1488f
Adham <muzaffarov.adham@gmail.com>
2021-06-09 14:17:23 +0500

    delete section ended

--- client/src/server.js
+++ client/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fetch = require('node-fetch')
 const path = require('path')
 const app = express()
 const host = 'localhost'
@@ -14,8 +15,26 @@ app.get('/incomes', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'incomes.html'))
 })
 
+app.get('/incomes/delete/:id', (req, res) => {
+    let { id } = req.params
+    fetch('http://localhost:8000/incomes/delete/' + id, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    }).then(res => res.json())
+    res.redirect('/incomes')
+})
+
 app.get('/expenses', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'expenses.html'))
+})
+
+app.get('/expenses/delete/:id', (req, res) => {
+    let { id } = req.params
+    fetch('http://localhost:8000/expenses/delete/' + id, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    }).then(res => res.json())
+    res.redirect('/expenses')
 })
 
 app.get('/total', (req, res) => {
